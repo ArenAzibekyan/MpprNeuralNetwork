2c130184c91aa39cd7992f97695bd51a242b21a5
Aren Azibekyan <aren.azibekyan@mail.ru>
2017-11-23 03:46:35 +0300

    making api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import recognizePhoto
+from .views import recognizePhoto, learnDigit, learnMnist
 
 
 app_name = 'api'
 
 urlpatterns = [
-    url(r'^recognizePhoto', recognizePhoto, name='recognizePhoto')
+    url(r'^recognizePhoto', recognizePhoto, name='recognizePhoto'),
+    url(r'^learnDigit', learnDigit, name='learnDigit'),
+    url(r'^learnMnist', learnMnist, name='learnMnist')
 ]
