97ea064a21282b047840903ee327318ef815fc43
MaxAvatar <maxavatar007@gmail.com>
2017-11-26 17:04:54 +0300

    commit

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ВАШ РЕПОЗИТОРИЙ ЗАХВАТИЛА ВООРУЖЁННАЯ ЛОЛЯ
-[![Аниме](https://i.imgur.com/PfM4lHj.jpg)]
+![Аниме](https://i.imgur.com/PfM4lHj.jpg)
 
 # Required packages:
 - Django
+- Keras
+- Numpy
 - Pillow
-- tensorflow==1.3.0
-- keras
+- TensorFlow==1.3.0
--- static/js/myScript.js
+++ static/js/myScript.js
@@ -1,182 +1,174 @@
 function getCookie(name) {
-	var cookieValue = null;
-	if (document.cookie && document.cookie != '') {
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = jQuery.trim(cookies[i]);
-			// Does this cookie string begin with the name we want?
-			if (cookie.substring(0, name.length + 1) == (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	return cookieValue;
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
 var csrftoken = getCookie('csrftoken');
 
 function csrfSafeMethod(method) {
-	// these HTTP methods do not require CSRF protection
-	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
 $.ajaxSetup({
-	beforeSend: function (xhr, settings) {
-		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-			xhr.setRequestHeader("X-CSRFToken", csrftoken);
-		}
-	}
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
 });
 
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
+var slider_flag = false;
 
-$('#learning-speed').slider({
-	step: 1,
-	tooltip: 'always',
-	tooltip_position: 'bottom'
+$(document).ready(function () {
+    $("#network-add-slider").bootstrapSlider();
+    $('#learning-speed').slider({
+        step: 1,
+        tooltip: 'always',
+        tooltip_position: 'bottom'
+    });
 });
 
 $('#result').css({
-	width: 14 * 40,
-	height: 14 * 40
+    width: 28 * 20,
+    height: 28 * 20
 })
 
 var pad = new Sketchpad({
-	element: '#pad',
-	width: 14 * 40,
-	height: 14 * 40
+    element: '#pad',
+    width: 28 * 20,
+    height: 28 * 20
 });
 
-pad.penSize = 40;
+pad.penSize = 20;
 pad.color = 'black';
 
 $('#undo').click(function () {
-	pad.undo();
+    pad.undo();
 });
 
 $('#redo').click(function () {
-	pad.redo();
+    pad.redo();
 });
 
 function draw_prediction(arr) {
-	arr.forEach(function (inner_array) {
-		inner_array.forEach(function (item, i) {
-			var value = parseInt(item * 100);
-			var myclass = "progress-bar progress-bar-striped progress-bar-success active";
-			if (66 >= value && value > 33) {
-				myclass = "progress-bar progress-bar-striped progress-bar-warning active";
-			}
-			if (33 >= value) {
-				myclass = "progress-bar progress-bar-striped progress-bar-danger active";
-			}
-			$('#progress-' + i).closest('div.row').attr("data-sort", value);
-			$('#progress-' + i).prop("class", myclass);
-			$('#progress-' + i).css('width', value + '%');
-			$('#progress-' + i).text(value + '%');
-		})
-	})
-	setTimeout(function () {
-		var $container = $('#result');
-		$container.find('.row').sort(function (a, b) {
-			return +b.getAttribute('data-sort') - +a.getAttribute('data-sort');
-		}).appendTo($container);
-	}, 600)
+    arr.forEach(function (inner_array) {
+        inner_array.forEach(function (item, i) {
+            var value = parseInt(item * 100);
+            var myclass = "progress-bar progress-bar-striped progress-bar-success active";
+            if (66 >= value && value > 33) {
+                myclass = "progress-bar progress-bar-striped progress-bar-warning active";
+            }
+            if (33 >= value) {
+                myclass = "progress-bar progress-bar-striped progress-bar-danger active";
+            }
+            $('#progress-' + i).closest('div.row').attr("data-sort", value);
+            $('#progress-' + i).prop("class", myclass);
+            $('#progress-' + i).css('width', value + '%');
+            $('#progress-' + i).text(value + '%');
+        })
+    })
+    setTimeout(function () {
+        var $container = $('#result');
+        $container.find('.row').sort(function (a, b) {
+            return +b.getAttribute('data-sort') - +a.getAttribute('data-sort');
+        }).appendTo($container);
+    }, 600)
 }
 
-setInterval(function () {
-	for (var i = 0; i < 10; i++) {
-		var value = getRandomInt(0, 100);
-		var myclass = "progress-bar progress-bar-striped progress-bar-danger active";
-		if (66 >= value && value > 33) {
-			myclass = "progress-bar progress-bar-striped progress-bar-warning active";
-		}
-		if (33 >= value) {
-			myclass = "progress-bar progress-bar-striped progress-bar-success active";
-		}
-		$('#progress-error-' + i).closest('div.row').attr("data-sort", value);
-		$('#progress-error-' + i).prop("class", myclass);
-		$('#progress-error-' + i).css('width', value + '%');
-		$('#progress-error-' + i).text(value + '%');
-	}
-	setTimeout(function () {
-		var $container = $('#errors');
-		$container.find('.row').sort(function (a, b) {
-			return +b.getAttribute('data-sort') - +a.getAttribute('data-sort');
-		}).appendTo($container);
-	}, 600)
-
-}, 1000);
-
 $('#recogniseSubmit').click(function () {
-	document.getElementById('pad').toBlob(function (e) {
-		var data = new FormData();
-		data.append('digitPhoto', e, 'image.png');
-		deferred = $.ajax({
-			type: 'POST',
-			processData: false,
-			contentType: false,
-			url: 'api/recognizeDigit',
-			data: data
-		});
-		deferred.done(function (response) {
-			if (!response.ok) {
-				console.log(response.error);
-			} else {
-				draw_prediction(response.values);
-			}
-		});
-		deferred.fail(function () {
-			console.log('Не удается распознать! Сервер недоступен');
-		});
-	});
+    document.getElementById('pad').toBlob(function (e) {
+        var data = new FormData();
+        data.append('digitPhoto', e, 'image.png');
+        deferred = $.ajax({
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            url: 'api/recognizeDigit',
+            data: data
+        });
+        deferred.done(function (response) {
+            if (!response.ok) {
+                console.log(response.error);
+            } else {
+                draw_prediction(response.values);
+            }
+        });
+        deferred.fail(function () {
+            console.log('Не удается распознать! Сервер недоступен');
+        });
+    });
 });
 
 // пример апи как слать фотку цифры на обучение и эту самую цифру
 
-// $('#teach').click(function() {
-//     document.getElementById('pad').toBlob(function(e) {
-//         var data = new FormData();
-//         data.append('digitPhoto', e, 'image.png');
-//         data.append('value', 2);
-//         deferred = $.ajax({
-//             type: 'POST',
-//             processData: false,
-//             contentType: false,
-//             url: 'api/learnDigit',
-//             data: data
-//         });
-//         deferred.done(function(response) {
-//             if (!response.ok) {
-//                 console.log(response.error);
-//             }
-//             // м
-//             // а
-//             // к
-//             // с
-//             // дальше брать проценты и отображать
-//             else {
-//                 console.log('success');
-//             }
-//         });
-//         deferred.fail(function() {
-//             console.log('Не удается распознать! Сервер недоступен');
-//         });
-//     });
-// });
+$('#recogniseAdd').click(function () {
+    $('#network-add-modal').modal('show');
+    if (!slider_flag) setTimeout(function () {
+        $("#network-add-slider").bootstrapSlider('refresh');
+        slider_flag = true;
+    }, 500)
+});
+
+$('#recognise-add-ok').click(function () {
+    document.getElementById('pad').toBlob(function (e) {
+        var data = new FormData();
+        data.append('digitPhoto', e, 'image.png');
+        data.append('value', $("#network-add-slider").val());
+        data.append('epochCount', 1)
+        deferred = $.ajax({
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            url: 'api/learnDigit',
+            data: data
+        });
+        deferred.done(function (response) {
+            if (!response.ok) {
+                console.log(response.error);
+            }
+            else {
+                console.log('success');
+            }
+        });
+        deferred.fail(function () {
+            console.log('Не удается распознать! Сервер недоступен');
+        });
+    });
+    $('#network-add-modal').modal('hide');
+});
 
 $('#teach').click(function () {
-	$.ajax({
-		type: 'GET',
-		url: 'api/learnMnist',
-		data: {
-			epochCount: $('#learning-speed').val()
-			// epochCount: 5
-		},
-		success: function (response) {
-			console.log(response);
-		}
-	})
+    $.ajax({
+        type: 'GET',
+        url: 'api/learnMnist',
+        data: {
+            epochCount: $('#learning-speed').val()
+            // epochCount: 5
+        },
+        success: function (response) {
+            console.log(response);
+        }
+    })
+});
+
+$('#reset').click(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'api/resetTrain',
+        success: function (response) {
+            console.log(response);
+        }
+    })
 });
